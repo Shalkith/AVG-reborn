3e61170552220b934eae38c37821e911d30c68a9
Paul <pgwar2@gmail.com>
2019-08-10 16:19:28 -0700

    upload AVG Beta site

---
+++ app.py
@@ -0,0 +1,238 @@
+from flask import Flask, render_template, request,redirect, url_for
+import string
+import os
+from scripts import creds, auther
+from datetime import datetime
+import flask_login
+
+
+
+
+import mysql.connector
+
+def dbcheck(query):
+    # Connect to server
+    cnx = mysql.connector.connect(
+        host=creds.mysqlhost,
+        port=3306,
+        user=creds.mysqlun,
+        password=creds.mysqlpw,
+        database='avidgamers')
+
+    # Get a cursor
+    cur = cnx.cursor()
+    # Execute a query
+    cur.execute(query)
+    result = cur
+    data = []
+    print(query)
+    for x in cur:
+        data.append(x)
+    try:
+        cnx.commit()
+    except:
+        pass
+    return data
+
+
+
+
+app = Flask(__name__)
+key = 'jUNrE-I|m,MI9Wr~nG7KK]8I%>zIuf{K*IlV<W<Y-m_Pi?U.w368S4YB+H*3cG8-nG7KK]8I%>zIuf{K*IlV<W<'
+app.secret_key = key
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+@login_manager.user_loader
+def user_loader(email):
+    user = User()
+    print('attempting to confirm',email,'is authed')
+
+    reauthdata = auther.verifyuser(email,'password')
+    print('Reauth',reauthdata)
+    user.id = reauthdata[1]
+    user.uniqueid = reauthdata[0]
+    user.role = reauthdata[2]
+    user.displayname = reauthdata[3]
+    user.uniqueidhash = reauthdata[4]
+    user.parentsite = reauthdata[5]
+
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email == None:
+        pass
+    else:
+        authdata = auther.verifyuser(email,password)
+    if email == None or authdata[6] !=True:
+        return
+    user = User()
+    user.id = authdata[1]
+    user.uniqueid = authdata[0]
+    user.role = authdata[2]
+    user.displayname = authdata[3]
+    user.uniqueidhash = authdata[4]
+    user.parentsite = authdata[5]
+    user.is_authenticated = authdata[6]
+
+    return user
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+            return render_template('login.html')
+
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        authdata = auther.verifyuser(email,password)
+        print(authdata)
+
+        if authdata[6] == True:
+            user = User()
+            user.id = authdata[1]
+            user.uniqueid = authdata[0]
+            user.role = authdata[2]
+            user.displayname = authdata[3]
+            user.uniqueidhash = authdata[4]
+            user.parentsite = authdata[5]
+
+
+            flask_login.login_user(user)
+            return redirect(url_for('hello'))
+    except Exception as e:
+        print(e)
+        pass
+    return render_template('login.html',message="Unauthorized. Bad Username or Password.")
+
+
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return render_template('login.html',message="Logged out.")
+
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return render_template('login.html',message="Unauthorized. Please login first.")
+
+
+
+###############
+
+
+@app.route('/posts/<variable>', methods=['GET','POST'])
+def posts(variable):
+    if request.method == 'POST':
+        postdata = request.values
+        postdata = postdata.to_dict()
+        now = str(datetime.now()).split('.')[0][:-3]
+
+        validusercheck = auther.confirm_poster(postdata['username'],postdata['unHa'],postdata['unId'])
+        print('validusercheck', validusercheck)
+        if validusercheck == True:
+            dbcheck('insert into posts (parentsite,subject,body,parentthread,postedby,lastupdate,createddate,type) values ("0","'+postdata['postsubject']+'","'+postdata['postbody']+'","'+variable+'","'+postdata['username']+'","'+now+'","'+now+'","post")')
+
+
+            board = dbcheck('select * from posts where id="'+variable+'"')
+            posts = dbcheck('select * from posts where parentsite="0" and (type = "post" and parentthread="'+variable+'") order by lastupdate desc')
+            print(posts)
+            #do your code here
+            return render_template("posts.html",data=posts, board=board)
+        else:
+            board = dbcheck('select * from posts where id="'+variable+'"')
+            posts = dbcheck('select * from posts where parentsite="0" and (type = "post" and parentthread="'+variable+'") order by lastupdate desc')
+
+            return render_template("posts.html",data=posts, board=board, message = 'somthing went wrong. please try again...')
+
+    else:
+
+        board = dbcheck('select * from posts where id="'+variable+'"')
+        posts = dbcheck('select * from posts where parentsite="0" and (type = "post" and parentthread="'+variable+'") order by lastupdate desc')
+        print(posts)
+        #do your code here
+        return render_template("posts.html",data=posts, board=board)
+
+@app.route('/thread/<variable>', methods=['GET','POST'])
+def thread(variable):
+    if request.method == 'POST':
+        postdata = request.values
+        postdata = postdata.to_dict()
+        now = str(datetime.now()).split('.')[0][:-3]
+        dbcheck('insert into posts (parentsite,subject,body,parentthread,postedby,lastupdate,createddate,type) values ("0","NULL","'+postdata['postreply']+'","'+variable+'","admin","'+now+'","'+now+'","reply")')
+        dbcheck('update posts set lastupdate = "'+now+'" where id ="'+variable+'"')
+        threads = dbcheck('select * from posts where id="'+variable+'" or (parentthread="'+variable+'" and type = "reply")')
+        board = dbcheck('select * from posts where id="'+variable+'"')
+        print(posts)
+        #do your code here
+        return render_template("thread.html",data=threads, board=board)
+
+    else:
+
+        threads = dbcheck('select * from posts where id="'+variable+'" or (parentthread="'+variable+'" and type = "reply")')
+        board = dbcheck('select * from posts where id="'+variable+'"')
+        print(posts)
+        #do your code here
+        return render_template("thread.html",data=threads, board=board)
+
+
+
+######### login goes up here ^
+app.debug=True
+
+class BenefitTemplateService(object):
+
+    @staticmethod
+    def create(params):
+        # some validation here
+
+        params['credit_behavior'] = "none"
+        return params
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return '404'
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return '500'
+    return render_template('404.html'), 404
+
+
+@app.route('/')
+def home():
+    var = dbcheck('select * from posts where parentsite="0" and parentthread="0"')
+    return render_template('home.html', data = var)
+
+@app.route('/post')
+def newpost():
+    return render_template('newpost.html')
+
+
+
+@app.route('/general')
+def gen():
+    return render_template('genboard.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
---
+++ scripts/auther.py
@@ -0,0 +1,81 @@
+from hashlib import sha1
+from scripts import creds
+
+
+def confirm_poster(username,hashid,unid):
+    import mysql.connector
+    u = creds.mysqlun # MySQL username
+    p = creds.mysqlpw # MySQL password
+    h = creds.mysqlhost # MySQL server
+    db = 'avidgamers' # MySQL datanase name
+
+    cnx = mysql.connector.connect(user=u, password=p,host=h,database=db)
+    cursor = cnx.cursor(buffered=True)
+    query = ("select username,id,email from users where username = '"+username+"'")
+    print (query)
+    cursor.execute(query)
+    validuser  = False
+    for x in cursor:
+        print('vaid user query',x)
+        if username == x[0] and (unid == str(x[1]) and hashid == x[2]):
+            validuser = True
+
+    return validuser
+
+def verifyuser(email,password):
+    import mysql.connector
+    #MySQL details. same for all scripts
+
+
+    u = creds.mysqlun # MySQL username
+    p = creds.mysqlpw # MySQL password
+    h = creds.mysqlhost # MySQL server
+    db = 'avidgamers' # MySQL datanase name
+
+    cnx = mysql.connector.connect(user=u, password=p,host=h,database=db)
+    cursor = cnx.cursor(buffered=True)
+    query = ("select * from users where username = '"+email+"'")
+    query2 = ("select username,password from users where username='"+email+"'")
+
+    cursor.execute(query)
+    validuser  = False
+
+    for x in cursor:
+        if x[1] == email and x[7] != 0:
+            #print('valid user')
+            validuser = True
+            id,username,role,displayname,useremail,parentsite,active = x[0],x[1],x[3],x[4],x[5],x[6],x[7]
+            #return True
+        elif x[1] == email and x[7] == 1:
+            print('disabled user')
+        else:
+            pass
+
+
+    if validuser != True:
+        return '','','','','','',False
+
+    cursor.execute(query2)
+
+    for x in cursor:
+        if x[0] == username:
+            username,hashedpassword = generate_mysql_hash(username,password)
+            if hashedpassword == x[1]:
+                #print('password confirmed. authed')
+                return id,username,role,displayname,useremail,parentsite,True
+            else:
+                #print('password failed. not authed')
+                return id,username,role,displayname,useremail,parentsite,False
+
+
+        else:
+            #print(x[0]+' is not valid!!!')
+            return '','','','','','',False
+
+    return '','','','','','',False
+
+
+def generate_mysql_hash(username,password):
+    mysql_hash = '*' + sha1(sha1(password.encode('utf-8')).digest()).hexdigest()
+
+    return username,mysql_hash
